1efd2c5be78318b93a8f9414b4bbf274b8437299
Mark Kanko <mkanko66@gmail.com>
2019-01-26 01:38:03 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i =0 
   even_collection = [] 
   while i < collection.length 
-    if (yield(collection[i]))
+    if yield(collection[i])
       even_collection << collection[i] 
     end 
     i += 1 

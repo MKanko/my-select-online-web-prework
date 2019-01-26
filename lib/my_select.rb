5bdcb572e38ac03ff857f26f3beb9e757f591547
Mark Kanko <mkanko66@gmail.com>
2019-01-26 01:34:54 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,17 +1,1 @@
-def my_select(collection)
-  
-  even_collection = []
-  
-  i = 0 
-  
-  while i < collection.length 
-  
-    even_collection << yield(collection[i]) 
-  
-    i = i + 1 
-    
-  end 
-  
-  even_collection
-  
-end
+
